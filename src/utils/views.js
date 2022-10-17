34da0e605d01b2618adbbfc54fe1e45f7b382d8b
shiipou <shiishii@nocturlab.fr>
2022-10-17 16:30:19 +0200

    feat: add static markdowns load
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- src/utils/views.js
+++ src/utils/views.js
@@ -3,15 +3,14 @@ const Path = require('path');
 const Utils = require('./common');
 const showdown  = require('showdown');
 const glob = require('glob');
-const { basename } = require('path');
+
+const config = require('../i18n/common.json')
 
 const converter = new showdown.Converter();
 
 const viewsPath = Path.join(__dirname, '..', 'views');
 const markdownPath = Path.join(__dirname, '..', 'markdown');
 const componentsApiBasePath = '/components-api/';
-
-// const componentsApiBasePath = '/';
 
 /**
  * Get the view file for the given path and language
@@ -31,23 +30,34 @@ function getViewFile(path, lang) {
 }
 
 async function getPages() {
+	const markdowns = Utils.getFilesRecursively(markdownPath);
+	const home_markdown = markdowns.splice(markdowns.findIndex(page => Path.basename(page, '.md') === 'index'), 1)[0]
 	const home = new Page('/', 'Documentation', "Lenra's documentation", 'layout');
+	home.content = converter.makeHtml(fs.readFileSync(home_markdown, 'utf8'));
 	home.collapsable = true;
 
-	const markdowns = Utils.getFilesRecursively(markdownPath)
+	const static_pages = markdowns
 		.filter(file => file.endsWith('.md'))
 		.map(file => {
-			const page = new Page(file.substring(file.lastIndexOf('/')).replace('.md', '.html'), Path.basename(file, '.md'), "Description is not managed yet", "layout");
+			const page = new Page(file.substring(file.lastIndexOf('/')).replace('.md', '.html'),
+				Path.basename(file, '.md'),
+				"Description is not managed yet",
+				"layout");
 			page.content = converter.makeHtml(fs.readFileSync(file, 'utf8'));
-			home.subPages.push(page)
+			const position = config.page[page.path]?.position
+			if (position !== undefined) {
+				home.subPages.splice(position, 0, page)
+			} else {
+				home.subPages.push(page)
+			}
 			return page
 		});
 
-	let all_pages = [home, ...markdowns];
+	let all_pages = [home, ...static_pages];
 
 	const api_dir = Path.join(__dirname, '../api/', componentsApiBasePath)
 	if ((await fs.stat(api_dir)).isDirectory()) {
-		home.subPages = await Promise.all(fs.readdirSync(api_dir).filter(api_name => !api_name.startsWith('.')).map(async api_name => {
+		home.subPages = home.subPages.concat(await Promise.all(fs.readdirSync(api_dir).filter(api_name => !api_name.startsWith('.')).map(async api_name => {
 			const api_path = Path.join(api_dir, api_name)
 			const api = new Page(`/${api_name}/`, api_name, 'Description is not managed yet', 'definition-summary');
 			api.collapsable = true
@@ -61,17 +71,15 @@ async function getPages() {
 				console.error(err);
 			});
 			api.subPages = components_paths
-				.map(component_path => new Page(Path.join(api.path, basename(component_path)), Path.basename(component_path, '.html'), 'Description is not managed yet', 'definition', fs.readFileSync(component_path).toString()));
+				.map(component_path => new Page(Path.join(api.path, Path.basename(component_path)), Path.basename(component_path, '.html'), 'Description is not managed yet', 'definition', fs.readFileSync(component_path).toString()));
 			all_pages = [...all_pages, api, ...api.subPages]
-
 			return api;
 		})).catch(err => {
 			console.error(err)
-		});
+		}));
 	} else {
 		console.error(`${api_dir} wasn't a correct directory. Make sure it exist`)
 	}
-
 	return all_pages;
 }
 
