a3ad8580a2503e5c57f79e9a215ff5b15403c88a
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-02-18 14:09:36 +0100

    feat: Use Lesta framework

--- lesta.config.js
+++ lesta.config.js
@@ -1,8 +1,11 @@
 import { Page, PageManager, RobotsManager, SitemapManager } from '@lenra/lesta';
 import { getFilesRecursively } from '@lenra/lesta/lib/utils.js';
 import * as Path from 'path';
+import * as fs from 'fs';
+import Showdown from 'showdown';
 
 const languageFileRegex = /^(.+)[.]([a-z]{2})([.](md|html))$/
+const converter = new Showdown.Converter();
 
 /**
  * Returns the website path managers
@@ -60,8 +63,8 @@ async function markdownPageLister(configuration) {
                 {
                     title,
                     description,
-                    content: "coucou"
-                    // content: converter.makeHtml(fs.readFileSync(file, 'utf8'))
+                    // TODO: get markdown content
+                    content: converter.makeHtml(fs.readFileSync(Path.join(markdownDirPath, file), 'utf8'))
                 }
             )
         })
@@ -73,5 +76,35 @@ async function markdownPageLister(configuration) {
  * @returns {Promise<Page[]>}
  */
 async function apiPageLister(configuration) {
-    return [];
+    const apisDirPath = Path.join(process.cwd(), "src/api");
+    const files = (await getFilesRecursively(apisDirPath)).filter(p => p.endsWith(".html"));
+    const relativeFiles = files.filter(file => !Path.basename(file).startsWith('.'))
+        .map(file => Path.relative(apisDirPath, file));
+    const langViews = {};
+    const matchFiles = relativeFiles.map(file => ({
+        file,
+        match: file.match(languageFileRegex)
+    }));
+    matchFiles.filter(({ match }) => match)
+        .forEach(({ file, match }) => {
+            const lang = match[2];
+            const f = match[1] + match[3];
+            if (!langViews[f]) langViews[f] = {};
+            langViews[f][lang] = file;
+        });
+    return matchFiles.filter(({ match }) => !match)
+        .map(({ file }) => {
+            const path = file;
+            const content = fs.readFileSync(Path.join(apisDirPath, file), 'utf8');
+            const json = JSON.parse(fs.readFileSync(Path.join(apisDirPath, `${file}.json`), 'utf8'));
+            return new Page(
+                path,
+                'layout.pug',
+                langViews[file] || {},
+                {
+                    ...json,
+                    content
+                }
+            )
+        })
 }
